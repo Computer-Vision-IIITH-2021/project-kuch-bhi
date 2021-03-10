2033f0dbceaeb31a72e798ea526e8d17d68b9e33
trizonxjay <trizonxjay@gmail.com>
2021-03-10 18:37:10 +0530

    added classifier/regressor architecture, classifier training

--- temp-code/RCNN/create_training_data.py
+++ temp-code/RCNN/create_training_data.py
@@ -21,47 +21,51 @@ def IoU(bbox1, bbox2):
 	union_area = area1 + area2 - intersection_area
 	return intersection_area/union_area
 
-images_path = "../../../VOC2012/JPEGImages"
-annots_path = "../../../VOC2012/Annotations"
-out_path = "../../../training_data"
-if not os.path.exists(out_path):
-	os.mkdir(out_path)
+def main():
+	images_path = "../../../VOC2012/JPEGImages"
+	annots_path = "../../../VOC2012/Annotations"
+	out_path = "../../../training_data"
+	if not os.path.exists(out_path):
+		os.mkdir(out_path)
 
-out_filename = lambda class_count_, class_index_: os.path.join(out_path, f'{class_index_}_{class_count_}.jpg')
+	out_filename = lambda class_count_, class_index_: os.path.join(out_path, f'{class_index_}_{class_count_}.jpg')
 
-NUM_IMAGES = 10
-LIMIT_CLASS_IMAGES = 30
-image_size = (224,224)
+	NUM_IMAGES = 10
+	LIMIT_CLASS_IMAGES = 30
+	image_size = (224,224)
 
-class_list = ['background','person','bird','cat','cow','dog','horse','sheep','aeroplane','bicycle','boat','bus','car','motorbike','train','bottle','chair','diningtable','pottedplant','sofa','tvmonitor']
-NUM_CLASSES = len(class_list)
-class_index_mapping = {}
-for classname in class_list:
-	class_index_mapping[classname] = class_list.index(classname)
+	class_list = ['background','person','bird','cat','cow','dog','horse','sheep','aeroplane','bicycle','boat','bus','car','motorbike','train','bottle','chair','diningtable','pottedplant','sofa','tvmonitor']
+	NUM_CLASSES = len(class_list)
+	class_index_mapping = {}
+	for classname in class_list:
+		class_index_mapping[classname] = class_list.index(classname)
 
-count = 0
-class_count = np.zeros((NUM_CLASSES), dtype='int')
-total_class_count = class_count.copy()
-for filename in tqdm(os.listdir(images_path)[:NUM_IMAGES]):
-	xml_filepath = os.path.join(annots_path, filename.split('.')[0] + '.xml') 
-	image_filepath = os.path.join(images_path, filename)
-	class_count *= 0
+	count = 0
+	class_count = np.zeros((NUM_CLASSES), dtype='int')
+	total_class_count = class_count.copy()
+	for filename in tqdm(os.listdir(images_path)[:NUM_IMAGES]):
+		xml_filepath = os.path.join(annots_path, filename.split('.')[0] + '.xml') 
+		image_filepath = os.path.join(images_path, filename)
+		class_count *= 0
 
-	classnames, boxes = parseXML(xml_filepath)
-	image = cv2.imread(image_filepath, cv2.IMREAD_COLOR)
+		classnames, boxes = parseXML(xml_filepath)
+		image = cv2.imread(image_filepath, cv2.IMREAD_COLOR)
 
-	for bbox in find_region_proposals(image):
-		x1,y1,x2,y2 = bbox
-		max_iou = 0
-		for classname, box in zip(classnames, boxes):
-			class_index = class_index_mapping[classname]
-			iou = IoU(bbox,box)
-			max_iou = max(max_iou,iou)
-			if iou > 0.7 and class_count[class_index]<LIMIT_CLASS_IMAGES:
-				cv2.imwrite(out_filename(total_class_count[class_index],class_index), cv2.resize(image[y1:y2,x1:x2], image_size))
-				class_count[class_index] += 1
-				total_class_count[class_index] += 1
-		if max_iou < 0.3 and class_count[0]<LIMIT_CLASS_IMAGES:
-			cv2.imwrite(out_filename(total_class_count[0],0), cv2.resize(image[y1:y2,x1:x2], image_size))
-			class_count[0] += 1
-			total_class_count[0] += 1
+		for bbox in find_region_proposals(image):
+			x1,y1,x2,y2 = bbox
+			max_iou = 0
+			for classname, box in zip(classnames, boxes):
+				class_index = class_index_mapping[classname]
+				iou = IoU(bbox,box)
+				max_iou = max(max_iou,iou)
+				if iou > 0.7 and class_count[class_index]<LIMIT_CLASS_IMAGES:
+					cv2.imwrite(out_filename(total_class_count[class_index],class_index), cv2.resize(image[y1:y2,x1:x2], image_size))
+					class_count[class_index] += 1
+					total_class_count[class_index] += 1
+			if max_iou < 0.3 and class_count[0]<LIMIT_CLASS_IMAGES:
+				cv2.imwrite(out_filename(total_class_count[0],0), cv2.resize(image[y1:y2,x1:x2], image_size))
+				class_count[0] += 1
+				total_class_count[0] += 1
+
+if __name__=="__main__":
+	main()
--- temp-code/RCNN/train.py
+++ temp-code/RCNN/train.py
@@ -1,9 +1,113 @@
+from torch.hub import import_module
 import torchvision.models as models
 from dataset import MyCustomDataset
+from torch.utils.data import DataLoader
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from tqdm import tqdm
+from datetime import datetime
+import os
+currentTime = lambda : datetime.now().strftime("%d-%m-%Y %H-%M-%S")
 
 DATA_PATH = '../../../training_data'
+BATCH_SIZE = 4
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+SEED = 0
+NUM_CLASSES = 21
+torch.manual_seed(SEED)
+img_size = (224,224)
+LEARNING_RATE = 1e-3
+NUM_EPOCHS = 10
+
+if not os.path.exists("checkpoints"):
+	os.mkdir("checkpoints")
 
 model = models.vgg16(pretrained=True)
+
+class Identity(nn.Module):
+	def __init__(self):
+		super(Identity, self).__init__()
+	def forward(self, x):
+		return x
+
+model.avgpool = Identity()
+model.classifier = nn.Sequential(
+	nn.Linear(in_features=25088, out_features=4096, bias=True),
+	nn.ReLU(inplace=True),
+	nn.Dropout(p=0.5, inplace=False),
+	nn.Linear(in_features=4096, out_features=4096, bias=True),
+	nn.ReLU(inplace=True),
+	nn.Dropout(p=0.5, inplace=False),
+	nn.Linear(in_features=4096, out_features=NUM_CLASSES, bias=True),
+)
+
+model.regressor = nn.Sequential(
+	nn.Linear(in_features=25088, out_features=4096, bias=True),
+	nn.ReLU(inplace=True),
+	nn.Dropout(p=0.5, inplace=False),
+	nn.Linear(in_features=4096, out_features=4096, bias=True),
+	nn.ReLU(inplace=True),
+	nn.Dropout(p=0.5, inplace=False),
+	nn.Linear(in_features=4096, out_features=4, bias=True),
+)
+
+model = model.to(DEVICE)
+for param in model.parameters():
+	param.requires_grad = False
+
 train_set = MyCustomDataset(data_dir=DATA_PATH,split="train")
 test_set = MyCustomDataset(data_dir=DATA_PATH,split="test")
+train_loader = DataLoader(train_set,batch_size=BATCH_SIZE,shuffle=True,drop_last=False)
+test_loader = DataLoader(test_set,batch_size=BATCH_SIZE,shuffle=True,drop_last=False)
 
+# ------------- ------------- ------------- -----
+# ------------- Training Classifier -------------
+# ------------- ------------- ------------- -----
+
+for param in model.classifier.parameters():
+	param.requires_grad = True
+
+optimizer = optim.Adam(model.classifier.parameters(),lr=LEARNING_RATE, betas=(0.9, 0.999), eps=1e-8)
+criterion = nn.CrossEntropyLoss()
+
+for epoch in range(NUM_EPOCHS):
+	train_loss = 0
+	with tqdm(train_loader, unit="batch", desc="Training", position=1, disable=False) as tepoch:
+		for i,(imgs,labels) in enumerate(tepoch):
+			imgs, labels = imgs.to(DEVICE), labels.to(DEVICE).long()
+			optimizer.zero_grad()
+			preds = model.classifier(model.features(imgs).reshape(BATCH_SIZE,-1))
+			loss = criterion(preds, labels)
+			loss.backward()
+			optimizer.step()
+			train_loss += loss.item()
+			tepoch.set_postfix(loss=train_loss/(i+1))
+	PATH = os.path.join("checkpoints/", f'classification {currentTime()} epoch-{epoch+1}.pth')
+	torch.save(model.state_dict(), PATH)
+
+# ------------- ------------- ------------- ----
+# ------------- Training Regressor -------------
+# ------------- ------------- ------------- ----
+
+# for param in model.regressor.parameters():
+# 	param.requires_grad = True
+
+# optimizer = optim.Adam(model.regressor.parameters(),lr=LEARNING_RATE, betas=(0.9, 0.999), eps=1e-8)
+# criterion = nn.MSELoss()
+
+# for epoch in range(NUM_EPOCHS):
+# 	train_loss = 0
+# 	with tqdm(train_loader, unit="batch", desc="Training", position=1, disable=False) as tepoch:
+# 		for i,(imgs,labels) in enumerate(tepoch):
+# 			imgs, labels = imgs.to(DEVICE), labels.to(DEVICE)
+# 			optimizer.zero_grad()
+# 			preds = model.classifier(model.features(imgs).reshape(BATCH_SIZE,-1))
+# 			loss = criterion(preds, labels)
+# 			loss.backward()
+# 			optimizer.step()
+# 			train_loss += loss.item()
+# 			tepoch.set_postfix(loss=train_loss/(i+1))
+# 	PATH = os.path.join("checkpoints/", f'classification {currentTime()} epoch-{epoch+1}.pth')
+# 	torch.save(model.state_dict(), PATH)
